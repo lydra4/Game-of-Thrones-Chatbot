9276e15f6a6f13c4663bed33968d63443549f2e1
Song Yi <lydra4@gmail.com>
2025-07-22 16:08:41 +0800

    feat: updated base agent

--- src/agent/base_agent.py
+++ src/agent/base_agent.py
@@ -1,7 +1,9 @@
 import logging
 from typing import Any, List, Optional, Union
 
+from langchain.agents import initialize_agent
 from omegaconf import DictConfig
+from langchain.tools import BaseTool
 
 
 class BaseAgent:
@@ -9,17 +11,19 @@ class BaseAgent:
         self,
         cfg: DictConfig,
         llm: Any,
-        input_data: Union[str, List[str]],
-        tools: Optional[List[Any]] = None,
+        tools: Optional[List[BaseTool]] = None,
         logger: Optional[logging.Logger] = None,
     ):
         self.cfg = cfg
         self.llm = llm
-        self.input_data = input_data
-        self.tools = tools
+        self.tools = tools or []
         self.logger = logger or logging.getLogger(__name__)
+        self.agent = initialize_agent(tools=self.tools, llm=self.llm, verbose=True)
 
-        self.logger.debug(f"{self.__class__.__name__} initialized with config: {cfg}")
+        self.logger.debug(f"{self.__class__.__name__} initialized with config: {cfg}")s
 
-    def run(self):
-        raise NotImplementedError("Each agent must implement it's own run method.")
+    def run(self, input_data: Union[str, List[str]]):
+        if not hasattr(self, 'agent'):
+            raise NotImplementedError("Each agent must implement its own run method.")
+        
+        return self.agent.run(input_data)
