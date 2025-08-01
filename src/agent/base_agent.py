b86f29a25141b7532f92289c8364983dd9d8086c
Song Yi <lydra4@gmail.com>
2025-08-01 14:00:23 +0800

    chore: change from agent to chain since not using tools

--- src/agent/base_agent.py
+++ src/agent/base_agent.py
@@ -2,10 +2,8 @@ import logging
 from abc import ABC, abstractmethod
 from typing import Any, List, Optional, Union
 
-from langchain.agents import AgentExecutor
-from langchain.agents.openai_tools.base import create_openai_tools_agent
-from langchain.tools import BaseTool
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnableSequence
 from omegaconf import DictConfig
 
 
@@ -14,30 +12,22 @@ class BaseAgent(ABC):
         self,
         cfg: DictConfig,
         llm: Any,
-        tools: Optional[List[BaseTool]] = None,
         logger: Optional[logging.Logger] = None,
     ):
         self.cfg = cfg
         self.llm = llm
-        self.tools = tools or []
         self.logger = logger or logging.getLogger(__name__)
-        self.agent = self._initialize_agent()
-
         self.logger.debug(f"{self.__class__.__name__} initialized with config: {cfg}")
 
-    def _initialize_agent(self):
+    def _initialize_chain(self) -> RunnableSequence:
         prompt = ChatPromptTemplate.from_template(
             template=self.cfg.template,
-            input_variables=self.cfg.input_variables,
-        )
-        agent = create_openai_tools_agent(
-            llm=self.llm,
-            tools=self.tools,
-            prompt=prompt,
         )
 
-        return AgentExecutor(agent=agent, tools=self.tools, verbose=True)
+        return prompt | self.llm
 
     @abstractmethod
     def run(self, input_data: Union[str, List[str]]):
-        pass
+        chain = self._initialize_chain()
+        output = chain.invoke(input=input_data)
+        return output
