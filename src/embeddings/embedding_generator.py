74ed6fa1010638cdb89b805744c7e6e38f4d46dc
Song Yi <lydra4@gmail.com>
2025-08-06 11:49:34 +0800

    chore: rename python script and class to better suit function, refactor code for unified vector space

--- src/embeddings/embedding_generator.py
+++ src/embeddings/embedding_generator.py
@@ -1,36 +1,46 @@
 import logging
 import os
 import re
-from typing import List, Optional
+from typing import Dict, List, Optional
 
+import torch
+from chromadb.utils.embedding_functions import OpenCLIPEmbeddingFunction
 from langchain.docstore.document import Document
 from langchain.text_splitter import (
     RecursiveCharacterTextSplitter,
     SentenceTransformersTokenTextSplitter,
 )
-from langchain_community.vectorstores import FAISS
+from langchain_chroma import Chroma
 from langchain_experimental.text_splitter import SemanticChunker
 from langchain_huggingface import HuggingFaceEmbeddings
 from omegaconf import DictConfig
 
 
-class TextEmbeddings:
+class GenerateEmbeddings:
     def __init__(
         self,
         cfg: DictConfig,
         documents: List[Document],
+        image_paths: List[str],
+        metadata_list: List[Dict[str, str]],
         logger: Optional[logging.Logger] = None,
     ) -> None:
         self.cfg = cfg
         self.documents = documents
-        self.logger = logger or logging.getLogger(__name__)
-        self.embeddings_model_name = re.sub(
-            r'[<>:"/\\|?*]', "_", self.cfg.text_embeddings.model_name
+        self.logger: logging.Logger = logger or logging.getLogger(__name__)
+        self.device: str = "cuda" if torch.cuda.is_available() else "cpu"
+        self.embedding_function = OpenCLIPEmbeddingFunction(
+            model_name=self.cfg.embeddings.model_name,
+            checkpoint=self.cfg.embeddings.checkpoint,
+            device=self.device,
         )
-        self.persist_directory = os.path.join(
+        self.cfg.embeddings.model_name_cleaned = re.sub(
+            r'[<>:"/\\|?*]', "_", self.cfg.embeddings.model_name
+        )
+        self.persist_directory: str = os.path.join(
             self.cfg.text_embeddings.embeddings_path,
             self.cfg.text_splitter.name,
-            self.embeddings_model_name,
+            self.cfg.embeddings.model_name_cleaned,
         )
 
         os.makedirs(self.persist_directory, exist_ok=True)
@@ -65,58 +75,24 @@ class TextEmbeddings:
 
         return documents
 
-    def _embed_documents(self) -> FAISS:
-        """
-        Embeds the documents using the loaded embedding model and saves them as a FAISS vector store.
-
-        The function ensures the text is split before embedding. It constructs an output path,
-        generates the embeddings, and saves them locally.
-
-        Returns:
-            FAISS: The FAISS vector store containing document embeddings.
-        """
-        if not self.texts:
-            self._split_text()
-
-        self.embeddings_model_name = re.sub(
-            r'[<>:"/\\|?*]',
-            "_",
-            self.cfg.embeddings.embeddings_model.model_name.split("/")[-1],
+    def _embed_documents(
+        self,
+        embedding_model: HuggingFaceEmbeddings,
+        documents: List[Document],
+    ):
+        self.logger.info(
+            f"Generating Embeddings, it will be save at {self.persist_directory}."
         )
 
-        self.embeddings_path = os.path.join(
-            self.cfg.embeddings.embed_documents.embeddings_path,
-            self.cfg.text_splitter.name,
-            self.embeddings_model_name,
-        )
-        os.makedirs(self.embeddings_path, exist_ok=True)
-        self.logger.info(f"Embeddings will be saved @ {self.embeddings_path}\n")
-
-        self.logger.info("Generating Vector Embeddings.\n")
-
-        vectordb = FAISS.from_documents(
-            documents=self.texts, embedding=self.embedding_model
+        Chroma.from_documents(
+            documents=documents,
+            embedding=embedding_model,
+            persist_directory=self.persist_directory,
         )
 
-        self.logger.info("Saving Vector Embeddings.\n")
+        self.logger.info("Successfully generated and saved Vector Embeddings.")
 
-        vectordb.save_local(
-            folder_path=self.embeddings_path,
-            index_name=self.cfg.embeddings.embed_documents.index_name,
-        )
-
-        self.logger.info("Successfully saved.\n")
-
-    def generate_vectordb(self) -> FAISS:
-        """
-        Generates and saves the vector store from the provided documents.
-
-        This is the public method that orchestrates the document splitting, embedding,
-        and FAISS vector store creation and saving.
-
-        Returns:
-            FAISS: The saved FAISS vector store containing embeddings.
-        """
+    def generate_vectordb(self):
         self.logger.info("Starting document processing and generating embeddings.\n")
         self._split_text()
         self._embed_documents()
